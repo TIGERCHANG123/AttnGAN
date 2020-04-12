d46b7a0467958586522d7f806c8c5db2baebf7c8
TIGERCHANG123 <2462924814@qq.com>
2020-04-12 09:52:32 +0800

    AttnGAN

--- AttnGAN.py
+++ AttnGAN.py
@@ -10,18 +10,17 @@ class embedding(tf.keras.Model):
     # text -> R(T)
     # code -> R(T * D_)
     code = self.embedding(text)
-    # whole_sequence_output1,2 -> R(T * D), final_memory_state1,2 -> R(D)
     whole_sequence_output_1, final_memory_state_1, final_carry_state_1 = self.lstm(code)
     whole_sequence_output_2, final_memory_state_2, final_carry_state_2 = self.go_backwards_lstm(code)
-
-    # whole_sequence_output -> R(T * 2D)
+    # whole_sequence_output1,2 -> R(T * D), final_memory_state1,2 -> R(D)
     whole_sequence_output = tf.concat([whole_sequence_output_1, whole_sequence_output_2], axis=-1)
-    # whole_sequence_output -> R(2D * T)
+    # whole_sequence_output -> R(T * 2D)
     whole_sequence_output = tf.transpose(whole_sequence_output, [0, 2, 1])
-    # final_memory_state -> R(2D)
+    # whole_sequence_output -> R(2D * T)
     final_memory_state = tf.concat([final_memory_state_1, final_memory_state_2], axis=-1)
-
-    return whole_sequence_output, final_memory_state# batch_size * (word_length * 2) * seq_length, batch size * (word length * 2)
+    # final_memory_state -> R(2D)
+    return whole_sequence_output, final_memory_state
+    # batch_size * (word_length * 2) * seq_length, batch size * (word length * 2)
 
 class generate_condition(tf.keras.Model):
   def __init__(self, units):
@@ -40,104 +39,85 @@ class generate_condition(tf.keras.Model):
 class Attn_generator(tf.keras.Model):
   def __init__(self):
     super(Attn_generator, self).__init__()
-    self.input_layer = Input(shape=[4, 4, 512], name='h0_input')
+    self.input_layer = generator_Input(shape=[4, 4, 1024], name='h0_input')
 
     self.deconv_list_1 = [
+      deconv(filters=512, strides=2, padding='same', name='h0_deconv1'),  # 256*8*8
       deconv(filters=256, strides=2, padding='same', name='h0_deconv1'),#256*8*8
       deconv(filters=128, strides=2, padding='same', name='h0_deconv2'),#128*16*16
       deconv(filters=64, strides=2, padding='same', name='h0_deconv3'),#64*32*32
     ]
     self.attention = attention(32, name='h1_attn')
     self.deconv_list_2 = [
-      Resdual_Block(64, name='h1_res1'),
-      Resdual_Block(64, name='h1_res2'),
-      deconv(filters=32, strides=2, padding='same', name='h1_deconv'),
+      Resdual_Block(128, name='h1_res1'),
+      Resdual_Block(128, name='h1_res2'),
+      deconv(filters=64, strides=2, padding='same', name='h1_deconv'),
     ]
-    self.output_0 = generator_Output(image_depth=3, strides=2, padding='same', name='h0_output')#3*64*64
-    self.output_1 = generator_Output(image_depth=3, strides=2, padding='same', name='h1_output')
+    self.output_0 = generator_Output(image_depth=3, strides=1, padding='same', name='h0_output')#3*64*64
+    self.output_1 = generator_Output(image_depth=3, strides=1, padding='same', name='h1_output')
   def call(self, sequence, text_embedding, noise):
-    # x -> R((D + 100))
+    # text_embedding -> R(D)
+    # noise -> R(100)
     x = tf.concat([noise, text_embedding], axis=1)
-    # h0 -> R(4 * 4 * 512)
+    # x -> R((D + 100))
     h0 = self.input_layer(x)
-    # h0 -> R(32 * 32 * 32)
+    # h0 -> R(4 * 4 * 1024)
     for i in range(len(self.deconv_list_1)):
       h0 = self.deconv_list_1[i](h0)
-    # h1 -> R(32 * 32 * 64)
+    # h0 -> R(64 * 64 * 32)
     h1 = self.attention(sequence, h0)
-    # h1 -> R(64 * 64 * 16)
+    # h1 -> R(64 * 64 * 64)
     for i in range(len(self.deconv_list_2)):
       h1 = self.deconv_list_2[i](h1)
-    # out0 -> R(64 * 64 * 3)
+    # h1 -> R(128 *128 * 32)
     out0 = self.output_0(h0)
-    # out1 -> R(128 * 128 * 3)
+    # out0 -> R(64 * 64 * 3)
     out1 = self.output_1(h1)
-
+    # out1 -> R(128 * 128 * 3)
     return out0, out1
 
 class Attn_discriminator(tf.keras.Model):
   def __init__(self):
     super(Attn_discriminator, self).__init__()
 
-    self.input_layer_0 = discriminator_Input(filters=32, kernel_size=4, strides=2, name='h0_input')
-    self.middle_list_0 = [
-      discriminator_Middle(kernel_size=4, filters=64, strides=2, padding='same', name='h0_middle1'),
-      discriminator_Middle(kernel_size=4, filters=128, strides=2, padding='same', name='h0_middle2'),
-      discriminator_Middle(kernel_size=4, filters=256, strides=2, padding='same', name='h0_middle3'),
-    ]
-    self.pre_output_0 = [
-      discriminator_Middle(kernel_size=4, filters=512, strides=2, padding='same', name='h0_pre_output'),
-      discriminator_Middle(kernel_size=3, filters=256, strides=1, padding='same', name='h0_pre_output'),
-    ]
+    self.input_layer_0 = discriminator_Input(filters=32, name='h0_input')
+    self.conv_0 = layers.Conv2D(filters=32*8,kernel_size=3, strides=1, name='h1_conv',
+                                       padding='same', use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
+    self.output_layer_0 = discriminator_Output(name='h0_output')#3*64*64
 
-    self.output_layer_0 = discriminator_Output(with_activation=False, name='h0_output')#3*64*64
-
-    self.input_layer_1 = discriminator_Input(filters=32, kernel_size=4, strides=2, name='h1_input')
-    self.middle_list_1 = [
-      discriminator_Middle(kernel_size=4, filters=64, strides=2, padding='same', name='h1_middle1'),  # 1024*4*4
-      discriminator_Middle(kernel_size=4, filters=128, strides=2, padding='same', name='h1_middle2'),  # 512*8*8
-      discriminator_Middle(kernel_size=4, filters=256, strides=2, padding='same', name='h1_middle3'),  # 256*16*16
-      discriminator_Middle(kernel_size=4, filters=512, strides=2, padding='same', name='h1_middle3'),  # 256*16*16
-    ]
-    self.pre_output_1 = [
-      discriminator_Middle(kernel_size=4, filters=1024, strides=2, padding='same',name='h1_pre_output'),
-      discriminator_Middle(kernel_size=3, filters=512, strides=1, padding='same',name='h1_pre_output')]
-    self.output_layer_1 = discriminator_Output(with_activation=False, name='h1_output')  # 3*64*64
+    self.input_layer_1 = discriminator_Input(filters=32, name='h1_input')
+    self.middle_1 = conv(kernel_size=4, filters=32*16, strides=2, padding='same', name='h1_middle')
+    self.conv_1 = layers.Conv2D(filters=32*8,kernel_size=3, strides=1, name='h1_conv',
+                                       padding='same', use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
+    self.output_layer_1 = discriminator_Output(name='h1_output')  # 3*64*64
   def call(self, text_embedding, image_0, image_1):
     # image_0 -> R(64 * 64 * 3)
-    # x0 -> R(32 * 32 * 32)
     x0 = self.input_layer_0(image_0)
     # x0 -> R(4 * 4 * 256)
-    for i in range(len(self.middle_list_0)):
-      x0 = self.middle_list_0[i](x0)
     ones0 = tf.ones(shape=[1, x0.shape[1], x0.shape[2], 1], dtype=x0.dtype)
-    # code -> R(D)
+    # text_embedding -> R(D)
     code = text_embedding
-    # code -> R(1 * 1 * D)
     code = tf.expand_dims(code, axis=1)
     code = tf.expand_dims(code, axis=2)
-    
-    # image_text0 -> R(8 * 8 * D)
+    # code -> R(1 * 1 * D)
     image_text0 = ones0 * code
-    # x0 -> R(8 * 8 * (256+D))
+    # image_text0 -> R(4 * 4 * D)
     x0 = tf.concat([x0, image_text0], axis=-1)
-    # x0 -> R(4 * 4 * 512)
-    for i in range(len(self.pre_output_0)):
-      x0 = self.pre_output_0[i](x0)
+    # x0 -> R(4 * 4 * (256+D))
+    x0 = self.conv_0(x0)
+    # x0 -> R(4 * 4 * (256+D))
     output0 = self.output_layer_0(x0)
 
     # image_1 -> R(128 * 128 * 3)
-    # x1 -> R(64 * 64 * 32)
     x1 = self.input_layer_1(image_1)
-    # x1 -> R(4 * 4 * 512)
-    for i in range(len(self.middle_list_1)):
-      x1 = self.middle_list_1[i](x1)
+    # x1 -> R(8 * 8 * 256)
+    x1 = self.middle_1(x1)
+    # x1 -> (4 * 4 * 1024)
     ones1 = tf.ones(shape=[1, x1.shape[1], x1.shape[2], 1], dtype=x1.dtype)
     image_text1 = ones1 * code
     x1 = tf.concat([x1, image_text1], axis=-1)
-    # x1 -> (4 * 4 * 1024)
-    for i in range(len(self.pre_output_1)):
-      x1 = self.pre_output_1[i](x1)
+    # x0 -> R(4 * 4 * (256+D))
+    x1 = self.conv_1(x1)
     output1 = self.output_layer_1(x1)
     return output0, output1
 
--- AttnGAN_Block.py
+++ AttnGAN_Block.py
@@ -12,9 +12,95 @@ class GLU(tf.keras.Model):
         nc = int(nc/2)
         return x[:, :, :, :nc] * tf.keras.activations.sigmoid(x[:, :, :, nc:])
 
-class Input(tf.keras.Model):
+
+class conv(tf.keras.Model):
+    def __init__(self, kernel_size, filters, strides, padding, name):
+        super(conv, self).__init__()
+        self.conv = tf.keras.layers.Conv2D(filters, kernel_size=kernel_size, strides=strides, name=name + '_conv',
+                                           padding=padding, kernel_initializer=RandomNormal(stddev=0.02))
+        self.bn = tf.keras.layers.BatchNormalization(momentum=0.9, name=name + '_bn')
+        self.leakyRelu = tf.keras.layers.LeakyReLU(alpha=0.2, name=name + '_leakyRelu')
+
+    def call(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.leakyRelu(x)
+        return x
+
+
+class deconv(tf.keras.Model):
+    def __init__(self, filters, strides, padding, name):
+        super(deconv, self).__init__()
+        self.conv = layers.Conv2DTranspose(filters, kernel_size=3,
+                                           strides=strides, padding=padding,
+                                           use_bias=False, name=name + '_conv',
+                                           kernel_initializer=RandomNormal(stddev=0.02))
+        self.bn = layers.BatchNormalization(momentum=0.9, name=name + '_bn')
+        # self.relu = tf.keras.layers.ReLU(name=name+'_relu')
+        self.relu = GLU()
+
+    def call(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+class Resdual_Block(tf.keras.Model):
+  def __init__(self, filters, name):
+      super(Resdual_Block, self).__init__()
+      self.conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=1, padding='same',
+                                         kernel_initializer=RandomNormal(stddev=0.02), name=name+'_conv1')
+      self.conv2 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=1, padding='same',
+                                         kernel_initializer=RandomNormal(stddev=0.02), name=name+'_conv2')
+      self.bn1 = tf.keras.layers.BatchNormalization(name=name+'_bn1')
+      self.bn2 = tf.keras.layers.BatchNormalization(name=name+'_bn2')
+      self.Relu1 = GLU()
+      # self.Relu2 = GLU()
+  def call(self, x):
+      # x -> R(w * h * c)
+      y = self.conv1(x)
+      # y -> R(w * h * filters)
+      y = self.bn1(y)
+      y = self.Relu1(y)
+      # y -> R(w * h * filters/2)
+      y = self.conv2(y)
+      # y -> R(w * h * filters/2)
+      y = self.bn2(y)
+      y = y + x
+      return y
+
+class attention(tf.keras.Model):
+  def __init__(self, units, name):
+    super(attention, self).__init__()
+    self.dense = layers.Dense(units=units, name=name+'_dense')
+  def call(self, e, h):
+    # e -> R(D*T), h -> R(h * w * D^)
+    e_ = tf.transpose(e, [0, 2, 1])
+    # e_ -> R(T * D)
+    e_ = self.dense(e_)
+    # e_ -> R(T * D^)
+    e_ = tf.transpose(e_, [0, 2, 1])
+    # e_ -> R(D^*T)
+    h1 = tf.transpose(h, [0, 3, 1, 2])
+    # h1 -> R(D^ * h * w)
+    h1 = tf.reshape(h1, [h1.shape[0], h1.shape[1], -1])
+    # h1 -> R(D^ * N)
+    s = tf.matmul(tf.transpose(e_, [0, 2, 1]), h1)
+    # s -> R(T * N)
+    beta = tf.nn.softmax(s, axis=1)
+    c = tf.matmul(e_, beta)
+    # c -> R(D^ * N)
+    c = tf.transpose(c, [0, 2, 1])
+    # c -> R(N * D^)
+    c = tf.reshape(c, h.shape)
+    # c -> R(h * w * D^)
+    output = tf.concat([c, h], axis=3)
+    # output -> R(h * w * 2D^)
+    return output
+
+class generator_Input(tf.keras.Model):
   def __init__(self, shape, name):
-    super(Input, self).__init__()
+    super(generator_Input, self).__init__()
     self.dense = layers.Dense(shape[0] * shape[1] * shape[2], use_bias=False, kernel_initializer=RandomNormal(stddev=0.02), name=name+'_dense')
     self.reshape = layers.Reshape([shape[0], shape[1], shape[2]], name=name+'_reshape')
     self.bn = layers.BatchNormalization(momentum=0.9, name=name+'_bn')
@@ -27,83 +113,10 @@ class Input(tf.keras.Model):
     x = self.relu(x)
     return x
 
-class attention(tf.keras.Model):
-  def __init__(self, units, name):
-    super(attention, self).__init__()
-    self.dense = layers.Dense(units=units, name=name+'_dense')
-  def call(self, e, h):# e -> R(D*T), h -> R(h * w * D^)
-    # e_ -> R(T * D)
-    e_ = tf.transpose(e, [0, 2, 1])
-    # print('e_ shape', e_.shape)
-    # e_ -> R(T * D^)
-    e_ = self.dense(e_)
-    # print('e_ shape', e_.shape)
-    # e_ -> R(D^*T)
-    e_ = tf.transpose(e_, [0, 2, 1])
-    # print('e_ shape', e_.shape)
-    # h1 -> R(D^ * h * w)
-    h1 = tf.transpose(h, [0, 3, 1, 2])
-    # h1 -> R(D^ * N)
-    h1 = tf.reshape(h1, [h1.shape[0], h1.shape[1], -1])
-    # print('h1 shape', h1.shape)
-    # s -> R(T * N)
-    s = tf.matmul(tf.transpose(e_, [0, 2, 1]), h1)
-    beta = tf.nn.softmax(s, axis=1)
-    # c -> R(D^ * N)
-    c = tf.matmul(e_, beta)
-    # c -> R(N * D^)
-    c = tf.transpose(c, [0, 2, 1])
-    # c -> R(h * w * D^)
-    c = tf.reshape(c, h.shape)
-    # output -> R(h * w * 2D^)
-    output = tf.concat([c, h], axis=3)
-    return output
-
-class Resdual_Block(tf.keras.Model):
-  def __init__(self, filters, name):
-      super(Resdual_Block, self).__init__()
-      self.conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=1, padding='same',
-                                         kernel_initializer=RandomNormal(stddev=0.02), name=name+'_conv1')
-      self.conv2 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=1, padding='same',
-                                         kernel_initializer=RandomNormal(stddev=0.02), name=name+'_conv2')
-      self.bn1 = tf.keras.layers.BatchNormalization(name=name+'_bn1')
-      self.bn2 = tf.keras.layers.BatchNormalization(name=name+'_bn2')
-      # self.Relu1 = tf.keras.layers.ReLU(name=name+'_relu1')
-      # self.Relu2 = tf.keras.layers.ReLU(name=name+'_relu2')
-      self.Relu1 = GLU()
-      # self.Relu2 = GLU()
-  def call(self, x):
-      y = self.conv1(x)
-      y = self.bn1(y)
-      y = self.Relu1(y)
-      y = self.conv2(y)
-      y = self.bn2(y)
-      y = y + x
-      # y = self.Relu2(y)
-      return y
-
-
-class deconv(tf.keras.Model):
-  def __init__(self, filters, strides, padding, name):
-      super(deconv, self).__init__()
-      self.conv = layers.Conv2DTranspose(filters, kernel_size=3,
-                                         strides=strides, padding=padding,
-                                         use_bias=False, name=name+'_conv',
-                                         kernel_initializer=RandomNormal(stddev=0.02))
-      self.bn = layers.BatchNormalization(momentum=0.9, name=name+'_bn')
-      # self.relu = tf.keras.layers.ReLU(name=name+'_relu')
-      self.relu = GLU()
-  def call(self, x):
-      x = self.conv(x)
-      x = self.bn(x)
-      x = self.relu(x)
-      return x
-
 class generator_Output(tf.keras.Model):
   def __init__(self, image_depth, strides, padding, name):
     super(generator_Output, self).__init__()
-    self.conv = layers.Conv2DTranspose(image_depth,
-                                       kernel_size=3, strides=strides, name=name+'_conv',
+    self.conv = layers.Conv2D(image_depth,kernel_size=3, strides=strides, name=name+'_conv',
                                        padding=padding, use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
     self.actv = layers.Activation(activation='tanh', name=name+'_tan')
   def call(self, x):
@@ -112,42 +125,33 @@ class generator_Output(tf.keras.Model):
     return x
 
 class discriminator_Input(tf.keras.Model):
-  def __init__(self, filters, kernel_size, strides, name):
+  def __init__(self, filters, name):
     super(discriminator_Input, self).__init__()
-    self.conv = keras.layers.Conv2D(filters, kernel_size=kernel_size, strides=strides, name=name+'_conv',
+    self.conv = keras.layers.Conv2D(filters, kernel_size=4, strides=2, name=name+'_conv',
                                     padding="same", kernel_initializer=RandomNormal(stddev=0.02))
     self.leakyRelu = keras.layers.LeakyReLU(alpha=0.2, name=name+'_leakyRelu')
-    # self.dropout = keras.layers.Dropout(0.3)
+    self.middle_list = [
+        conv(kernel_size=4, filters=filters*2, strides=2, padding='same', name=name+'middle1'),
+        conv(kernel_size=4, filters=filters*4, strides=2, padding='same', name=name+'middle2'),
+        conv(kernel_size=4, filters=filters*8, strides=2, padding='same', name=name+'middle3'),
+    ]
   def call(self, x):
+    # x -> R(w * h * c)
     x = self.conv(x)
+    # x -> R(w/2 * h/2 * filters)
     x = self.leakyRelu(x)
-    # x = self.dropout(x)
+    for i in range(len(self.middle_list)):
+      x = self.middle_list_0[i](x)
+    # x -> R(w/16 * h/16 * filters*8)
     return x
 
-class discriminator_Middle(tf.keras.Model):
-  def __init__(self, kernel_size, filters, strides, padding, name):
-      super(discriminator_Middle, self).__init__()
-      self.conv = tf.keras.layers.Conv2D(filters, kernel_size=kernel_size, strides=strides, name=name+'_conv',
-                                         padding=padding, kernel_initializer=RandomNormal(stddev=0.02))
-      self.bn = tf.keras.layers.BatchNormalization(momentum=0.9, name=name+'_bn')
-      self.leakyRelu = tf.keras.layers.LeakyReLU(alpha=0.2, name=name+'_leakyRelu')
-      # self.dropout = tf.keras.layers.Dropout(0.3)
-  def call(self, x):
-      x = self.conv(x)
-      x = self.bn(x)
-      x = self.leakyRelu(x)
-      # x = self.dropout(x)
-      return x
-
 class discriminator_Output(tf.keras.Model):
-  def __init__(self, with_activation, name):
+  def __init__(self, name):
       super(discriminator_Output, self).__init__()
+
       self.flatten = tf.keras.layers.Flatten(name=name+'_flatten')
-      if with_activation:
-        self.dense = tf.keras.layers.Dense(units=1, activation='sigmoid',
+      self.dense = tf.keras.layers.Dense(units=1, activation='sigmoid',
                                            name=name+'output', kernel_initializer=RandomNormal(stddev=0.02))
-      else:
-        self.dense = tf.keras.layers.Dense(1, name=name+'output')
   def call(self, x):
       y1 = self.flatten(x)
       y1 = self.dense(y1)
